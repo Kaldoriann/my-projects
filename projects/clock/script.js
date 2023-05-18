5a29fdb0fabbb0ab93cb2751031153813252d940
Alex <alex.sizov96@gmail.com>
2023-05-18 15:49:09 +0300

    clock

---
+++ projects/clock/script.js
@@ -0,0 +1,30 @@
+// DOM elements
+const hourEl = document.getElementById("hour");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+//Get local time values
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  // Set AM/PM
+  let ampm = "AM";
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourEl.innerText = h;
+  minutesEl.innerText = m;
+  secondsEl.innerText = s;
+  ampmEl.innerText = ampm;
+  setTimeout(updateClock, 1000);
+}
+updateClock();
